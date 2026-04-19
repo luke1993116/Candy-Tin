e478f314350723163538e779f3a14910b599ada7
luke1993116 <lu2117719840@gmail.com>
2026-04-19 00:03:04 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'graviboxes-v37';
+const CACHE = 'graviboxes-v40';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-180.png'];
 
 self.addEventListener('install', e => {
